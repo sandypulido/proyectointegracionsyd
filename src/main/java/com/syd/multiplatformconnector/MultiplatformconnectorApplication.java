ad3fd4c1a1c96e2db21b9b6429143e6694b94726
Sandra Pulido Galván <nem.sandrap@coppel.com>
2024-12-22 08:23:08 -0600

    Inicializa el proyecto multiplatformconnector

---
+++ src/main/java/com/syd/multiplatformconnector/MultiplatformconnectorApplication.java
@@ -0,0 +1,13 @@
+package com.syd.multiplatformconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplatformconnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplatformconnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syd/multiplatformconnector/MultiplatformconnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syd.multiplatformconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplatformconnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
